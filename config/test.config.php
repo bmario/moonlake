bdc579523ec3619a9bdbe096bf0a3cf5737e0234
Mario Bielert <mario@moonlake.de>
2010-01-17 23:43:20 +0100

    Added some phpunit testcases

---
+++ config/test.config.php
@@ -0,0 +1,12 @@
+<?php
+
+$_CONFIG['test1'] = 'test1';
+$_CONFIG['test2'] = 'test2';
+$_CONFIG['test3'] = 'test3';
+$_CONFIG['test4'] = 'test4';
+$_CONFIG['test5'][] = 'test5.0';
+$_CONFIG['test5'][] = 'test5.1';
+$_CONFIG['test5'][] = 'test5.2';
+$_CONFIG['test5'][] = 'test5.3';
+
+?>
---
+++ tests/Default_LoaderTest.php
@@ -0,0 +1,57 @@
+<?php
+
+
+require_once 'moonlake/autoload/autoloader.class.php';
+require_once 'moonlake/autoload/loader/default.loader.php';
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * Default_Loader test case.
+ */
+class Default_LoaderTest extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @var Default_Loader
+	 */
+	private $Default_Loader;
+		
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp() {
+		parent::setUp ();
+		
+		$this->Default_Loader = new Default_Loader(/* parameters */);
+	
+	}
+	
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+
+		$this->Default_Loader = null;
+		
+		parent::tearDown ();
+	}
+	
+	public function classnames() {
+		return array(
+			array('Moonlake_Anything_Loader','moonlake/anything/loader.class.php'),
+			array('Anything_Loader', ''),
+			array('Something_Anything_Loader', '')
+		);
+	}
+	
+	/**
+	 * @dataProvider classnames
+	 */
+	public function testClassPath($name, $path) {
+
+		$this->assertEquals($path, $this->Default_Loader->classPath($name));
+	
+	}
+
+}
+
