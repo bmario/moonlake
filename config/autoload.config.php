6001dbbac9974521500edeb7445905d912a29f5a
Mario Bielert <mario@moonlake.de>
2010-03-05 01:30:23 +0100

    Added config for autoloader

--- config/autoload.config.php
+++ config/autoload.config.php
@@ -1,9 +1,10 @@
 <?php
 
-$_CONFIG[] = 'Default_Loader';
+$_CONFIG[] = 'Default_Loader'; // this can load mainly every class which is stored in the library
 $_CONFIG[] = 'Controller_Loader';
-$_CONFIG[] = 'View_Loader';
 $_CONFIG[] = 'Model_Loader';
-
+$_CONFIG[] = 'Exception_Loader';
+$_CONFIG[] = 'Validator_Loader';
+$_CONFIG[] = 'ViewHelper_Loader';
 
 ?>
